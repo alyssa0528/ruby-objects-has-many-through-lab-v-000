ef17c4f0a44e316c285355b4b9914d74bc3ed06a
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-03 14:36:33 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,11 @@
 class Patient
   attr_accessor :name 
   
-  def initialize (name)
+  def initialize(name)
     @name = name
     @appointments = []
   end   
+  
+  def add_appointment(appointment)
+  end 
 end 
