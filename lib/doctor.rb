e8568682d0c9c5417396e243b43c432caa319fce
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-03 14:42:27 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,7 +6,7 @@ class Doctor
     @appointments = []
   end 
   
-  def add_appointments(appointment.new)
+  def add_appointments(appointment)
     @appointments << appointment 
     appointment.doctor = self 
   end 
